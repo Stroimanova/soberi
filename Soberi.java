8f841be32a8fd7530e581a1813973dc88b7f457f
unknown <simonastroimanova@hotmail.com>
2023-12-04 22:53:27 +0100

    first java program

---
+++ Soberi.java
@@ -0,0 +1,15 @@
+package proekt_9;
+
+public class Soberi {
+
+	public static void main(String[] args) {
+		int sum = Soberi_broevi(2, 3, 4);
+		System.out.println("Sumata na trite broja = " + sum);
+	}
+	public static int Soberi_broevi(int a, int b, int c) {
+		return (a+b+c);
+	}
+}
+	
+		
+	
